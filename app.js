2dfc1f038695be5f57e887cceb45b4b35649a69e
Deonte Williams <icode4lyfe@gmail.com>
2018-10-04 04:54:50 -0400

    sets up basic functions

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const fs = require("fs");
+const yargs = require("yargs");
 const _ = require("lodash");
 
 const notes = require("./notes.js");
 
+let argv = yargs.argv;
+console.log(argv);
+let command = process.argv[2].toLowerCase();
+
+if (command === "add") {
+   notes.addNote(argv.title, argv.body);
+} else if (command === "list") {
+   notes.getAll();
+}  else if (command === "read") {
+   notes.getNote(argv.title);
+}  else if (command === "remove") {
+   notes.removeNote(argv.title);
+} else {
+   console.log("Command not recognized");
+}
--- notes.js
+++ notes.js
@@ -1,4 +1,22 @@
-module.exports.addNote = () => {
-   console.log("add note");
-   return "New Note";
+let addNote = (title, body) => {
+   console.log("Adding note.", title, body);
 };
+
+let getAll = () => {
+   console.log("Getting all notes.");
+};
+
+let getNote = (title) => {
+   console.log("Getting note.", title)
+};
+
+let removeNote = (title) => {
+   console.log("Removing note", title);
+}
+
+module.exports = {
+   addNote,
+   getAll,
+   getNote,
+   removeNote
+}
